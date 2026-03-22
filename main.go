befb3ca3b5c7bf5d9776e459c371dcad91ff97c5
Alan Night <a.nightails@gmail.com>
2026-03-22 07:02:35 -0500

    Introduce `player` struct with basic draw functionality
    - Draw player as a circle

--- main.go
+++ main.go
@@ -2,17 +2,43 @@ package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
+type circleShape interface {
+	draw()
+	update()
+	collidesWith(circleShape) bool
+}
+
+type player struct {
+	position rl.Vector2
+	velocity rl.Vector2
+	radius   float32
+	rotation float32
+}
+
+func (p *player) draw() {
+	rl.DrawCircleLinesV(p.position, p.radius, rl.White)
+}
+
 func main() {
 	rl.InitWindow(800, 450, "raylib [core] example - basic window")
 	defer rl.CloseWindow()
 
+	player := player{
+		position: rl.Vector2{X: 400, Y: 250},
+		radius:   10,
+		rotation: 0,
+	}
+
 	rl.SetTargetFPS(60)
 
 	for !rl.WindowShouldClose() {
+		// Update
+
+		// Draw
 		rl.BeginDrawing()
 		{
-			rl.ClearBackground(rl.RayWhite)
-			rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+			rl.ClearBackground(rl.DarkGray)
+			player.draw()
 		}
 		rl.EndDrawing()
 	}
